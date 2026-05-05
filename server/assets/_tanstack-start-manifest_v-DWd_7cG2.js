6e6f74053ef60215cf2dd25cb6e6cc49706d4e86
Muhammad Fahad Ismail <bsce2446@pieas.edu.pk>
2026-05-05 15:25:20 +0500

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-DWd_7cG2.js
@@ -0,0 +1,20 @@
+//#region \0tanstack-start-manifest:v
+var tsrStartManifest = () => ({
+	routes: {
+		__root__: {
+			filePath: "C:/Users/afsha/Downloads/PHIXZEN/phixzen-zen-clarity-care-main/src/routes/__root.tsx",
+			children: ["/"],
+			assets: void 0,
+			preloads: ["/phixzen/assets/index-BkcxWu7_.js"]
+		},
+		"/": {
+			filePath: "C:/Users/afsha/Downloads/PHIXZEN/phixzen-zen-clarity-care-main/src/routes/index.tsx",
+			children: void 0,
+			assets: void 0,
+			preloads: ["/phixzen/assets/routes-DjGHj69e.js"]
+		}
+	},
+	clientEntry: "/phixzen/assets/index-BkcxWu7_.js"
+});
+//#endregion
+export { tsrStartManifest };
